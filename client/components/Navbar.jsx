cc0fc4613ca8917379c09510f45b5adddfc31260
Michelle Wu <111860906+mich-wu@users.noreply.github.com>
2022-11-12 10:11:34 +1300

    update css

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>NAVBAR.JSX</h1>
+      <h1>Forkknife Wu 🍴</h1>
       <div className="links">
-        <a href="/">Home</a>
         <a href="/about">About Wu</a>
         <a href="/skins">Skins Collection</a>
       </div>
