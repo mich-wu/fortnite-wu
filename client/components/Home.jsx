7099313a0ed67308de8debd94420672cbdf353d3
Michelle Wu <111860906+mich-wu@users.noreply.github.com>
2022-11-12 19:59:14 +1300

    dancing duck default and lorem ipsum

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,7 +1,102 @@
 import React, { useState } from 'react'
 
 const Home = () => {
-  return <div className="home"></div>
+  return (
+    <>
+      <div className="home">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut pharetra sit amet
+        aliquam id diam. Dictumst quisque sagittis purus sit amet volutpat
+        consequat. Urna nec tincidunt praesent semper feugiat nibh sed pulvinar
+        proin. Posuere morbi leo urna molestie. Vitae justo eget magna fermentum
+        iaculis. Purus viverra accumsan in nisl nisi scelerisque eu. Aliquet
+        enim tortor at auctor urna. Mattis nunc sed blandit libero volutpat sed
+        cras ornare. Diam quam nulla porttitor massa id neque aliquam. Orci
+        nulla pellentesque dignissim enim. Quam adipiscing vitae proin sagittis
+        nisl. Urna neque viverra justo nec ultrices. Pharetra sit amet aliquam
+        id diam maecenas ultricies mi. Ultrices neque ornare aenean euismod
+        elementum nisi quis eleifend. At imperdiet dui accumsan sit amet nulla
+        facilisi morbi tempus. Et magnis dis parturient montes nascetur
+        ridiculus mus. Non arcu risus quis varius quam quisque id diam. Sem
+        viverra aliquet eget sit. Mattis aliquam faucibus purus in. Eget mi
+        proin sed libero enim sed faucibus turpis in. Nibh tortor id aliquet
+        lectus proin nibh nisl condimentum id. Malesuada nunc vel risus commodo
+        viverra maecenas accumsan. Aliquam vestibulum morbi blandit cursus risus
+        at ultrices mi. Et netus et malesuada fames ac turpis egestas. Quam
+        lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit.
+        Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
+        Eleifend mi in nulla posuere. Interdum consectetur libero id faucibus
+        nisl tincidunt eget. Odio euismod lacinia at quis risus sed vulputate.
+        Vel elit scelerisque mauris pellentesque pulvinar. Tellus at urna
+        condimentum mattis pellentesque id. Phasellus vestibulum lorem sed risus
+        ultricies tristique nulla. Ligula ullamcorper malesuada proin libero
+        nunc. Nulla pharetra diam sit amet nisl suscipit adipiscing bibendum.
+        Bibendum enim facilisis gravida neque convallis a. Id venenatis a
+        condimentum vitae sapien pellentesque. Rutrum tellus pellentesque eu
+        tincidunt tortor aliquam nulla. Proin nibh nisl condimentum id venenatis
+        a condimentum vitae sapien. Ullamcorper morbi tincidunt ornare massa
+        eget egestas purus. Consequat interdum varius sit amet mattis vulputate.
+        Elementum nisi quis eleifend quam adipiscing vitae proin. Vulputate
+        sapien nec sagittis aliquam malesuada. Risus feugiat in ante metus
+        dictum at. Eget lorem dolor sed viverra. Ultricies mi eget mauris
+        pharetra et ultrices neque ornare. Arcu cursus vitae congue mauris
+        rhoncus aenean vel elit scelerisque. Tellus pellentesque eu tincidunt
+        tortor. Ullamcorper morbi tincidunt ornare massa eget. Justo nec
+        ultrices dui sapien eget. Nisi est sit amet facilisis. Blandit volutpat
+        maecenas volutpat blandit aliquam etiam erat velit scelerisque. A
+        condimentum vitae sapien pellentesque habitant morbi tristique. Integer
+        feugiat scelerisque varius morbi. Id porta nibh venenatis cras sed felis
+        eget. Dui id ornare arcu odio ut. Eleifend mi in nulla posuere
+        sollicitudin aliquam ultrices sagittis. Integer vitae justo eget magna
+        fermentum. Accumsan in nisl nisi scelerisque. Et malesuada fames ac
+        turpis egestas integer eget aliquet nibh. Lacus viverra vitae congue eu
+        consequat ac. Dui sapien eget mi proin sed. Urna duis convallis
+        convallis tellus id interdum velit laoreet. Sed faucibus turpis in eu mi
+        bibendum. Diam maecenas sed enim ut sem. Nulla facilisi nullam vehicula
+        ipsum a arcu cursus vitae congue. Cras fermentum odio eu feugiat pretium
+        nibh ipsum. Volutpat consequat mauris nunc congue nisi vitae. Eu sem
+        integer vitae justo eget magna fermentum iaculis. Donec pretium
+        vulputate sapien nec sagittis aliquam malesuada. Egestas purus viverra
+        accumsan in nisl nisi scelerisque eu. Eu sem integer vitae justo eget
+        magna fermentum. Turpis egestas sed tempus urna et. Viverra ipsum nunc
+        aliquet bibendum enim. Sem viverra aliquet eget sit amet tellus cras. Ut
+        enim blandit volutpat maecenas volutpat blandit aliquam etiam erat.
+        Vestibulum morbi blandit cursus risus at ultrices mi. Id neque aliquam
+        vestibulum morbi. Pretium nibh ipsum consequat nisl vel. Pharetra magna
+        ac placerat vestibulum. Tempor nec feugiat nisl pretium fusce id velit.
+        Cursus risus at ultrices mi tempus imperdiet nulla. Elementum sagittis
+        vitae et leo duis ut diam. Vitae sapien pellentesque habitant morbi
+        tristique senectus et netus et. Vitae congue eu consequat ac felis.
+        Turpis nunc eget lorem dolor sed viverra ipsum nunc. Ultricies lacus sed
+        turpis tincidunt id aliquet risus feugiat in. Urna cursus eget nunc
+        scelerisque. Magna fermentum iaculis eu non diam. Nisl nunc mi ipsum
+        faucibus vitae aliquet nec ullamcorper sit. Ac felis donec et odio
+        pellentesque diam volutpat commodo. Tellus in hac habitasse platea
+        dictumst vestibulum. Nunc eget lorem dolor sed viverra ipsum. Facilisis
+        sed odio morbi quis commodo odio aenean sed adipiscing. Tortor consequat
+        id porta nibh. Vestibulum rhoncus est pellentesque elit ullamcorper
+        dignissim. Ac auctor augue mauris augue neque gravida in fermentum.
+        Vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae.
+        Gravida rutrum quisque non tellus. Amet justo donec enim diam. Leo a
+        diam sollicitudin tempor. Lacus viverra vitae congue eu consequat ac
+        felis donec et. Turpis in eu mi bibendum neque egestas congue quisque.
+        Etiam tempor orci eu lobortis elementum nibh tellus. Sociis natoque
+        penatibus et magnis dis parturient montes. Tortor consequat id porta
+        nibh venenatis cras. In hac habitasse platea dictumst quisque sagittis
+        purus sit. Nisl tincidunt eget nullam non nisi est sit amet. Leo integer
+        malesuada nunc vel risus. Magna ac placerat vestibulum lectus mauris
+        ultrices eros in. Sit amet mauris commodo quis imperdiet massa tincidunt
+        nunc pulvinar. Risus feugiat in ante metus dictum at tempor. A
+        scelerisque purus semper eget. Massa enim nec dui nunc mattis. Porta
+        nibh venenatis cras sed felis eget velit aliquet sagittis. Rhoncus est
+        pellentesque elit ullamcorper dignissim cras tincidunt. Neque sodales ut
+        etiam sit amet nisl. Erat pellentesque adipiscing commodo elit at
+        imperdiet dui accumsan sit. Magna ac placerat vestibulum lectus mauris
+        ultrices. Arcu odio ut sem nulla pharetra diam. In hac habitasse platea
+        dictumst quisque sagittis purus sit amet.
+      </div>
+    </>
+  )
 }
 
 export default Home
--- client/components/Skins.jsx
+++ client/components/Skins.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const Skins = () => {
   return (
     <div className="skins">
-      <h1>My locker below</h1>
+      <h1>My locker below:</h1>
       <p>I have spent way too much money on this game lol</p>
+      <img src="./images/defaultdance.gif" alt="default-dance"></img>
     </div>
   )
 }
