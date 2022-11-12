f79fab6e155246644c81df0ff32a18119f2b6cc9
Michelle Wu <111860906+mich-wu@users.noreply.github.com>
2022-11-12 22:30:10 +1300

    add api key... i think

--- client/apiClient.js
+++ client/apiClient.js
@@ -1,5 +1,11 @@
 import request from 'superagent'
 
-export function getGreeting() {
-  return request.get('/greeting').then((res) => res.body.greeting)
+// export function getGreeting() {
+//   return request.get('/greeting').then((res) => res.body.greeting)
+// }
+
+export function getItemShop() {
+  return request.get('https://fortnite-api.com/v2/shop/br').then((response) => {
+    return response.body
+  })
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const path = require('path')
 const express = require('express')
 const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const apiKey = process.env.FORNITE_API_KEY
 
 const server = express()
 
