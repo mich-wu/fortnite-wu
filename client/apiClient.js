c6b39c25f29a12ead867b64bbcb43e3bad85fe9d
Michelle Wu <111860906+mich-wu@users.noreply.github.com>
2022-11-13 20:04:45 +1300

    try map through items but fail lol

--- client/apiClient.js
+++ client/apiClient.js
@@ -2,7 +2,7 @@ import request from 'superagent'
 
 export function getItemShop() {
   return request.get('/api/v1/store').then((response) => {
-    console.log(response.body)
+    // console.log(response.body)
     return response.body
   })
 }
--- client/components/ItemShop.jsx
+++ client/components/ItemShop.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react'
-
 import { getItemShop } from '../apiClient'
 
 export default function ItemShop() {
-  const [shopData, setShopData] = useState({})
+  const [shopData, setShopData] = useState(null)
 
   useEffect(() => {
     getItemShop()
-      .then((items) => {
-        setShopData(items)
+      .then((currentItemShop) => {
+        console.log('current item shop:', currentItemShop)
+        setShopData(currentItemShop)
       })
       .catch((err) => {
         err.message
@@ -19,7 +19,14 @@ export default function ItemShop() {
     <>
       <div>
         <h1>Todays item shop below!</h1>
-        <h1>{shopData.name}</h1>
+        {/* {setShopData.map((item, i) => {
+          return (
+            <ul key={i}>
+              <h1>{shopData.name}</h1>
+              <h1>{shopData.vBucks}</h1>
+            </ul>
+          )
+        })} */}
       </div>
     </>
   )
