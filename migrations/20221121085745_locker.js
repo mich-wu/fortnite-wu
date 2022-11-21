a2625879f61749a1d71be3dbf3594d3c6a8ad52c
Michelle Wu <111860906+mich-wu@users.noreply.github.com>
2022-11-21 22:09:19 +1300

    db with seed data made

---
+++ migrations/20221121085745_locker.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('locker', (table) => {
+    table.increments('id')
+    table.string('name')
+    table.string('rarity')
+    table.integer('vbucks')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('locker')
+}
