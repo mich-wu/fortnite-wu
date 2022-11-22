c619b21d331739441fea7dfff9f65e02618decd5
Michelle Wu <111860906+mich-wu@users.noreply.github.com>
2022-11-22 17:36:14 +1300

    continue additem.jsx

--- client/components/AddItem.jsx
+++ client/components/AddItem.jsx
@@ -1,16 +1,26 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 
-function AddItem(){
-  
+function AddItem() {
+  const dispatch = useDispatch()
+  const [newItem, setNewItem] = useState({
+    url: ' ',
+    name: ' ',
+    rarity: ' ',
+    vbucks: ' ',
+  })
 
+  function handleChange(event) {
+    const { name, value } = event.target
+    setNewItem((result) => {
+      return { ...result, [name]: value }
+    })
+  }
 
-
-
-
-
-
-
-
-  .
+  function handleSubmit(event) {
+    event.preventDefault()
+    dispatch
+  }
 }
+
+export default AddItem
