accd550f1de9db9c67de78a8e0e44fc751aa0320
Michelle Wu <111860906+mich-wu@users.noreply.github.com>
2022-11-22 15:54:07 +1300

    db api working!!!

--- server/routes/locker.js
+++ server/routes/locker.js
@@ -1,2 +1,18 @@
 const express = require('express')
-const db = require('./locker')
+const db = require('../db/locker')
+
+const router = express.Router()
+
+//GET view all locker contents
+router.get('/', (req, res) => {
+  db.getLocker()
+    .then((contents) => {
+      res.json(contents)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ message: 'Something went wrong' })
+    })
+})
+
+module.exports = router
