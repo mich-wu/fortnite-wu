5d8bc4538dc09e42af87169903d50ef16fb414e4
Michelle Wu <111860906+mich-wu@users.noreply.github.com>
2022-11-22 22:31:26 +1300

    form working but locker not displaying

--- client/components/Locker.jsx
+++ client/components/Locker.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Locker = () => {
+  const displayLocker = useSelector((state) => state.locker)
+
   return (
-    <div className="locker">
-      <h1>My locker below:</h1>
+    <div className="home-grid">
+      <h1>KITAHANKYU'S LOCKER</h1>
+
       <p>I have spent way too much money on this game lol</p>
-      <img src="./images/defaultdance.gif" alt="default-dance"></img>
+      {/* <img src="./images/defaultdance.gif" alt="default-dance"></img> */}
+
+      {displayLocker?.map((item) => {
+        return (
+          <div key={item.id}>
+            <h2>{item.name}</h2>
+
+            <img alt={item.name} src={`${item.url}`} />
+          </div>
+        )
+      })}
     </div>
   )
 }
