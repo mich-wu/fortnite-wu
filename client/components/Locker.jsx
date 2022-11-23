df0a0290e87085a367f19b82c84a9ef046e3fe5f
Michelle Wu <111860906+mich-wu@users.noreply.github.com>
2022-11-23 22:53:37 +1300

    style locker and form page

--- client/components/Locker.jsx
+++ client/components/Locker.jsx
@@ -14,12 +14,14 @@ const Locker = () => {
   return (
     <>
       <h1>I spend too much money on this game lol</h1>
-      <div className="skins">
+      <div className="lockerskins">
         {lockerReducer?.map((item) => {
           return (
             <div key={item.id}>
               <img alt={item.name} src={item.url} className="lockerimage" />
               <h2>{item.name}</h2>
+              <p>{item.rarity}</p>
+              <p>{item.vbucks} vBucks</p>
             </div>
           )
         })}
