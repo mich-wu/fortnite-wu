ec52475c99dc8ea3abbe1ba6c4559e4b9d742264
Michelle Wu <111860906+mich-wu@users.noreply.github.com>
2022-12-19 12:28:04 +1300

    remove build from client and moved to root

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -30,7 +30,7 @@ const path = require('path')
 module.exports = {
   entry: path.join(__dirname, 'index.js'),
   output: {
-    path: path.join(__dirname, 'build'),
+    path: path.join(__dirname, '../build'),
     filename: 'bundle.js',
   },
   mode: 'production',
